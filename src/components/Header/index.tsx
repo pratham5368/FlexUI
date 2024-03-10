22fdf17663c88bbb533157b4566ef4d7877aa493
pratham rai <97867973+pratham5368@users.noreply.github.com>
2024-03-10 17:13:14 +0530

    fixed bug and removed light button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,18 @@
 "use client"; 
   
 // Import useState from 'react' library 
-import { useState } from "react"; 
+import { useState } from "react";
 import { Switch } from "../UI/Switch";
 
 function index() {
-  const [checked, setChecked] = useState(false);
+  {/*const [checked, setChecked] = useState(false);*/}
   return (
     <nav className="sticky top-0 z-10 bg-transparent">
      <div className="max-w-5xl mx-auto ">
      <div className="flex items-center justify-between h-16 ">
       <span className="text-2xl text-gray-100 font-semibold">Logo</span>
       <div className="flex space-x-4 text-white-100">
-      <Switch checked={checked} setChecked={setChecked} />
+      {/*<Switch checked={checked} setChecked={setChecked} />*/}
         <a href="#">Dashboard</a>
         <a href="#">About</a>
         <a href="#">Projects</a>
